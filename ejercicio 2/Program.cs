740c759087dceeb65eddf27698417cd8ff5d287d
Lindys Arely Martinez Herrera <herrerarely2003@gmail.com>
2022-08-29 22:10:21 -0600

    ejercicio 2

--- ejercicio 2/Program.cs
+++ ejercicio 2/Program.cs
@@ -5,21 +5,25 @@ namespace ejercicio_2
     class Program
     {
         static void Main(string[] args)
-        int[,] numeros = new int[3, 4] { { 5, 6, 1, 7 }, { 8, 7, 9, 4 }, { 3, 5, 44, 1 } };
-        forech(int i in numeros)
         {
-
-            Console.WriteLine(i);
-        }
-        int[,] array2 = new int[3, 3];
-        for (int i = 0; i < array2.GetLength (0); i++)
-        {
-            for (int j = 0; j<array2.GetLength (1); j++)
+            int[,] numeros = new int[3, 4] { { 5, 6, 1, 7 }, { 8, 7, 9, 4 }, { 3, 5, 4, 1 } };
+            foreach (int i in numeros)
             {
-            array2[i, j] = i* j;
-            Console.WriteLine(array2[i, j] + "");
+                Console.WriteLine(i);
             }
-            
+            int[,] array2 = new int[3, 3];
+            for (int i = 0; i < array2.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2.GetLength(1); j++)
+                {
+                    array2[i, j] = i * j;
+                    Console.Write(array2[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
 
+            string[,] paises = { { " EL Salvador", "Guatemala", "Honduras" }, { "", "", "" } };
+
+        }
     }
 }

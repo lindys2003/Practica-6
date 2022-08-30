68d9d02c1282aefcc0f43dc21d5b67c04119e6f2
Lindys Arely Martinez Herrera <herrerarely2003@gmail.com>
2022-08-29 22:31:07 -0600

    ejercicio 3

--- ejercicio 3/Program.cs
+++ ejercicio 3/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Collections;
 
-namespace ejercicio_3
+namespace programa_3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            arraylist lista = new arraylist();
+            ArrayList lista = new ArrayList();
             Console.WriteLine("cantidad: {0", lista.Count);
             Console.WriteLine("capacidad: [0", lista.Capacity);
             Console.WriteLine();
@@ -20,7 +21,7 @@ namespace ejercicio_3
             Console.WriteLine("capacidad: [0", lista.Capacity);
             Console.WriteLine();
 
-            for (int i = 10; i < 20; i++) ;
+            for (int i = 10; i < 20; i++) 
             {
                 lista.Add("dato + i");
             }
@@ -48,6 +49,8 @@ namespace ejercicio_3
             lista.Clear();
             Console.WriteLine("cantidad: {0}", lista.Count);
             Console.WriteLine("capacidad: {0]", lista.Capacity);
+
+
         }
     }
 }
